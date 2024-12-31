ac9e7da138a63586d08d210ff2aa554997d387a5
charan <parugula.saicharan@gmail.com>
2024-12-31 14:16:49 +0530

    first commit

---
+++ hashmap/src/module-info.java
@@ -0,0 +1,10 @@
+module hashmap1 {
+	import java.util.*;
+	public static void main(string[] args) {
+	scanner sc=new scanner(system.in);	
+	}
+	create hashmap<string,interger>map=newhashmap<string,integer>
+	fcount=
+	
+	
+}
